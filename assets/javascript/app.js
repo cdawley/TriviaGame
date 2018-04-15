529580536d301e0cc6de8af7f10cf161a23ee252
Christian Dawley <christian@art-box.us>
2018-04-14 22:48:57 -0400

    removed comment, console.log lines

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,14 +36,12 @@ let gameObj = {
     currNation: "",
     flagImgURL: "",
 
-    // timer stuff.... uncoded
 
+    //object containing timer variables/functions
     timer: {
 
-        //  Variable that will hold our setInterval that runs the stopwatch
         intervalId: 0,
 
-// prevents the clock from being sped up unnecessarily
     clockRunning: false,
 
     time: 10,
@@ -55,8 +53,6 @@ let gameObj = {
     },
 
     start: function() {
-        //console.log("start - " + this);
-        // DONE: Use setInterval to start the count here and set the clock to running.
         if (!this.clockRunning) {
             this.intervalId = setInterval(this.count, 1000);
             this.clockRunning = true;
@@ -68,23 +64,21 @@ let gameObj = {
         this.clockRunning = false;
     },
     count: function() {
-        console.log("count - " + gameObj.timer.time);
-            // DONE: decrement time by 1, remember we cant use "this" here.
+            // decrement time by 1, "remember we cant use "this" here."
             gameObj.timer.time -= 1;
 
             if (gameObj.timer.time === 0) {
                 gameObj.timer.timesUp();
-                gameObj.timer.stop();
-                gameObj.timer.reset();
             }
 
-            // DONE: Use the variable we just created to show the converted time in the "display" div.
             $("#time-remaining").text(gameObj.timer.time + "s");
         },
 
         timesUp: function() {
 
-            console.log("TIMES UP!")
+            gameObj.timer.stop();
+            gameObj.timer.reset();
+
 
         },
     },
@@ -99,49 +93,31 @@ let gameObj = {
     },
 
     ajaxFlagImg: function () {
-        console.log("ajax called");
-
-        let nation = this.currNation; //change this to take from array of country names
+        let nation = this.currNation;
         let queryURL = "https://en.wikipedia.org/api/rest_v1/page/summary/" + nation;
-
-        /*    notes from stack overflow on setting user-agent (asked for in API docs)
-            ------------------------------------------------------------------------
-                $.ajax({
-                    url: "http://localhost:4000",
-                    type: "POST",
-                    data: {we: "2"},
-                    dataType:'text',
-                    headers: {"User-Agent": "user agent 1"}
-                    beforeSend: function (req) {
-                        req.setRequestHeader('User-Agent', 'user agent changed to 2');
-                    } */
-
-        // from class exercise code
-
-        // Creates AJAX call for the specific movie button being clicked
-        // note: Wikipedia's API docs ask that contact information be given in User-Agent request header
-        // $(document).ready( function () {
-
         $.ajax({
             url: queryURL,
             method: "GET",
-            // the following was requested in API docs, but Google Chrome doesn't allow it
+
+            // API docs ask that developers put contact info in headers,
+            // Google Chrome will not allow this...
+
             // headers: {"User-Agent": "contact christian@art-box.us"},
+
         }).then(function (response) {
             this.flagImgURL = response.thumbnail.source;
             $("img#flag-img").attr("src", this.flagImgURL); // change flag image to current nation
-            console.log(gameObj.currNation + "\n" + gameObj.arrNationsUnused.length + " countries remaining");
+            console.log("correct answers is: " + gameObj.currNation + "\n" + gameObj.arrNationsUnused.length);
         });
     },
 
     randomAnswers: function () {
         /*
-     multiply random position (between 0 and 1/3 of all the countries)
-     by current iteration (from 1 to 3), and element at that index to a new array, provided its not the same
-     as the actual answer (if it is the same, take it from that index less 10)...
-     this should provide a diverse group of random "wrong answers"
-        */
-        this.arrAnswers.length = 0;
+     multiply random position (random number between 0 and 1/3 the size of array containing all countries)
+     by current iteration (from 1 to 3). check that element is not the same as current country, and push element
+     at current position/index to new array. (if element is the same subtract 1 from index & push that element).
+     finally, push current answer to new array.
+     */
 
         let startPos = Math.floor(Math.random() * Math.floor(this.arrNationsAll.length / 3));
 
@@ -149,39 +125,33 @@ let gameObj = {
 
             if (this.arrNationsAll[startPos * i] != this.currNation) {
                 this.arrAnswers.push(this.arrNationsAll[startPos * i]);
-                // set answer list item for each here??
-                console.log(this.arrAnswers[i - 1]);
             } else {
-                this.arrAnswers.push(this.arrNationsAll[(startPos * i) - 10]);
+                this.arrAnswers.push(this.arrNationsAll[(startPos * i) - 1]);
                 // or here if random answer is the same as current flag
-                console.log(this.arrAnswers[i - 1]);
             }
         }
 
         this.arrAnswers.push(this.currNation);
-
-
-        /* select random start position (between 1 & 3) and fill answer array from that index
-
-        *** note: this was a chunk of abstraction that took quite some time to chip away at *** */
     },
 
     chooseRandom: function() {
 
+        /*
+            select random start position (between 1 & 3) and fill answer array from that index
+            note: this was a chunk of abstraction that took quite some time to chip away at
+            */
+
         let Pos2 = Math.floor(Math.random() * Math.floor(4)); //random number between 0 and 3
-        console.log("start position is: " + Pos2);
         let answerID = 1;
         for (let i = Pos2; i < Pos2 + this.arrAnswers.length; i++) {
 
             let theIndex;
             if (i < this.arrAnswers.length) {
                 theIndex = i;
-                console.log("li#answer-" + (answerID));
                 $("li#answer-" + answerID).text(this.arrAnswers[theIndex]);
                 answerID++;
             } else {
                 theIndex = (this.arrAnswers.length - i) * -1;
-                console.log("li#answer-" + (answerID));
                 $("li#answer-" + answerID).text(this.arrAnswers[theIndex]);
                 answerID++
             }
